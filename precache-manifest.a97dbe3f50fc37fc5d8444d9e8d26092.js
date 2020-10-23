b335992698ccd1fe53cf6dacb361ad9cb9d939c5
Mehedi Haque <kpopdw@gmail.com>
2020-10-22 21:15:29 -0400

    Updates

---
+++ precache-manifest.a97dbe3f50fc37fc5d8444d9e8d26092.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a1751e3e47ae8e765f7601d126b40d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "63e9d0b0cd5e498a11ab",
+    "url": "/pokedex/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "f4055305931a8de1b10a",
+    "url": "/pokedex/static/css/main.5dc7c43e.chunk.css"
+  },
+  {
+    "revision": "63e9d0b0cd5e498a11ab",
+    "url": "/pokedex/static/js/2.41f4b361.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pokedex/static/js/2.41f4b361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4055305931a8de1b10a",
+    "url": "/pokedex/static/js/main.3acf67f7.chunk.js"
+  },
+  {
+    "revision": "dd6bddbc3b7842f642db",
+    "url": "/pokedex/static/js/runtime-main.ed7addec.js"
+  },
+  {
+    "revision": "f7b41b0b170d8765d7f6684497c7763a",
+    "url": "/pokedex/static/media/Pokeball.f7b41b0b.png"
+  }
+]);
